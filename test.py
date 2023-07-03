45686190d024a3370345a3bb432ac290b4b90377
Philippe Sanio <psanio10@gmail.com>
2023-07-03 16:08:30 -0500

    Update: test

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
 from main import Main
 
-a = 1
-b = 1
+
 
 # test
 def test_equal(a,b):
     assert Main.test_equal(a,b)
 
-if __name__ == "__main__":
-    test_equal(a,b)
+a = 1
+b = 1
+test_equal(a,b)
