4a733e2416b8b4ded5e56ce3b094c75325cc4bfe
Philippe Sanio <psanio10@gmail.com>
2023-07-03 15:40:40 -0500

    Added: Test

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("This is a test")
+class Main:
+    def __init__(self):
+        pass
+
+    @classmethod
+    def run(cls):
+        print("This is a test in main.py")
+
+    @classmethod
+    def test_equal(cls, a, b) -> bool:
+        return a == b
+
+# main
+if __name__ == "__main__":
+    Main.run()
---
+++ test/test.py
@@ -0,0 +1,8 @@
+from .main import Main
+
+a = 1
+b = 1
+
+# test
+def test_equal(a,b):
+    assert Main.test_equal(a,b)
